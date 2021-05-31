5a9f24527f8cc3d62b4e02ed3f85f385f6111e58
Thanh Le <mihthanh27@gmail.com>
2021-05-31 23:46:02 +0700

    Add preset topic

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,11 +3,194 @@
 	import { onMount } from 'svelte';
 	import { writable } from 'svelte/store';
 	import { Functions, Messaging } from '../lib';
+	import { uniq } from 'lodash';
 
-	let topics: string[] = JSON.parse(
-		'["Age","Alone","Amazing","Anger","Anniversary","Architecture","Art","Attitude","Beauty","Best","Birthday","Brainy","Business","Car","Chance","Change","Christmas","Communication","Computers","Cool","Courage","Dad","Dating","Death","Design","Diet","Dreams","Easter","Education","Environmental","Equality","Experience","Experience","Failure","Faith","Family","Famous","Father\'s Day","Fear","Finance","Fitness","Food","Forgiveness","Freedom","Friendship","Funny","Future","Gardening","God","Good","Government","Graduation","Great","Happiness","Health","History","Home","Hope","Humor","Imagination","Independence","Inspirational","Intelligence","Jealousy","Jealousy","Knowledge","Leadership","Learning","Legal","Life","Love","Marriage","Medical","Memorial Day","Men","Mom","Money","Morning","Mother\'s Day","Motivational","Movies","Moving On","Music","Nature","New Year\'s","Parenting","Patience","Patriotism","Peace","Pet","Poetry","Politics","Positive","Power","Relationship","Religion","Religion","Respect","Romantic","Sad","Saint Patrick\'s Day","Science","Smile","Society","Space","Sports","Strength","Success","Sympathy","Teacher","Technology","Teen","Thankful","Thanksgiving","Time","Travel","Trust","Truth","Valentine\'s Day","Veterans Day","War","Wedding","Wisdom","Women","Work"]'
-	);
-	let isInstalledWorker = navigator.serviceWorker.controller?.state === 'activated';
+	let topics: string[] = [
+		'Age',
+		'Alone',
+		'Amazing',
+		'Anger',
+		'Anniversary',
+		'Architecture',
+		'Art',
+		'Attitude',
+		'Beauty',
+		'Best',
+		'Birthday',
+		'Brainy',
+		'Business',
+		'Car',
+		'Chance',
+		'Change',
+		'Christmas',
+		'Communication',
+		'Computers',
+		'Cool',
+		'Courage',
+		'Dad',
+		'Dating',
+		'Death',
+		'Design',
+		'Diet',
+		'Dreams',
+		'Easter',
+		'Education',
+		'Environmental',
+		'Equality',
+		'Experience',
+		'Experience',
+		'Failure',
+		'Faith',
+		'Family',
+		'Famous',
+		"Father's Day",
+		'Fear',
+		'Finance',
+		'Fitness',
+		'Food',
+		'Forgiveness',
+		'Freedom',
+		'Friendship',
+		'Funny',
+		'Future',
+		'Gardening',
+		'God',
+		'Good',
+		'Government',
+		'Graduation',
+		'Great',
+		'Happiness',
+		'Health',
+		'History',
+		'Home',
+		'Hope',
+		'Humor',
+		'Imagination',
+		'Independence',
+		'Inspirational',
+		'Intelligence',
+		'Jealousy',
+		'Jealousy',
+		'Knowledge',
+		'Leadership',
+		'Learning',
+		'Legal',
+		'Life',
+		'Love',
+		'Marriage',
+		'Medical',
+		'Memorial Day',
+		'Men',
+		'Mom',
+		'Money',
+		'Morning',
+		"Mother's Day",
+		'Motivational',
+		'Movies',
+		'Moving On',
+		'Music',
+		'Nature',
+		"New Year's",
+		'Parenting',
+		'Patience',
+		'Patriotism',
+		'Peace',
+		'Pet',
+		'Poetry',
+		'Politics',
+		'Positive',
+		'Power',
+		'Relationship',
+		'Religion',
+		'Religion',
+		'Respect',
+		'Romantic',
+		'Sad',
+		"Saint Patrick's Day",
+		'Science',
+		'Smile',
+		'Society',
+		'Space',
+		'Sports',
+		'Strength',
+		'Success',
+		'Sympathy',
+		'Teacher',
+		'Technology',
+		'Teen',
+		'Thankful',
+		'Thanksgiving',
+		'Time',
+		'Travel',
+		'Trust',
+		'Truth',
+		"Valentine's Day",
+		'Veterans Day',
+		'War',
+		'Wedding',
+		'Wisdom',
+		'Women',
+		'Work'
+	];
+
+	const preset: Record<string, typeof topics[number][]> = {
+		productive: [
+			'Positive',
+			'Work',
+			'Communication',
+			'Inspirational',
+			'Amazing',
+			'Leadership',
+			'Time',
+			'Learning',
+			'Success'
+		],
+		motivational: [
+			'Positive',
+			'Inspirational',
+			'Positive',
+			'Amazing',
+			'Time',
+			'Fitness',
+			'Knowledge',
+			'Life',
+			'Future',
+			'Wisdom',
+			'Smile',
+			'Success'
+		],
+		'enjoy life': [
+			'Beauty',
+			'Friendship',
+			'Courage',
+			'Peace',
+			'Positive',
+			'Thanksgiving',
+			'Forgiveness',
+			'Health',
+			'Humor',
+			'Equality',
+			'Love',
+			'Dating',
+			'Smile',
+			'Sad',
+			'Religion'
+		],
+		knowledge: [
+			'Travel',
+			'Learning',
+			'Freedom',
+			'Experience',
+			'Intelligence',
+			'Technology',
+			'Wisdom',
+			'Science',
+			'Knowledge',
+			'Trust',
+			'Business'
+		]
+	};
+
 	const formValue = writable<ISettings>({
 		tag: [],
 		time: 'alltimes',
@@ -16,6 +199,7 @@
 		enabled: true
 	});
 	let isLoading = false;
+	let presetTag: Array<keyof typeof preset> = [];
 
 	onMount(() => {
 		if (browser) {
@@ -26,6 +210,24 @@
 			}
 		}
 	});
+
+	function toggleTag(tag: string) {
+		if (presetTag.includes(tag)) {
+			presetTag = presetTag.filter((item) => item !== tag);
+		} else {
+			presetTag = [...presetTag, tag];
+		}
+
+		let tags = [];
+		presetTag.forEach((item) => {
+			console.log({ item });
+			const listTags = preset[item] || [];
+
+			tags = [...tags, ...listTags];
+		});
+
+		$formValue.tag = uniq(tags);
+	}
 
 	function getSettings(token: string) {
 		Functions().then((instance) => {
@@ -133,26 +335,52 @@
 		<div class="p-6 card bordered space-y-6">
 			<div>
 				<h2 class="card-title">Pick your interested topics</h2>
-				<div class="grid grid-cols-2 md:grid-cols-4">
-					{#each topics as topic}
-						<div class="form-control flex-row">
-							<!-- svelte-ignore a11y-label-has-associated-control -->
-							<label class="cursor-pointer label space-x-4">
-								<div>
-									<input
-										type="checkbox"
-										name="tag"
-										bind:group={$formValue.tag}
-										value={topic}
-										checked={true}
-										class="radio checkbox checkbox-secondary"
-									/>
-									<span class="checkbox-mark" />
-								</div>
-								<span class="label-text capitalize">{topic}</span>
-							</label>
-						</div>
+				<div class="space-x-4">
+					{#each Object.keys(preset) as item}
+						<button
+							class="btn btn-primary capitalize"
+							class:btn-outline={!presetTag.includes(item)}
+							on:click|preventDefault={() => toggleTag(item)}>{item}</button
+						>
 					{/each}
+					<button
+						class="btn btn-secondary"
+						class:btn-outline={!presetTag.includes('customize')}
+						on:click|preventDefault={() => toggleTag('customize')}>Customize...</button
+					>
+				</div>
+
+				<div
+					class="collapse collapse-arrow mt-6"
+					class:collapse-open={presetTag.includes('customize')}
+				>
+					<!-- <input type="checkbox" />
+					<div class="collapse-title text-xl font-medium !bg-transparent m-auto">
+						Let me customize
+					</div> -->
+					<div class="collapse-content p-0 !bg-transparent">
+						<div class="grid grid-cols-2 md:grid-cols-4">
+							{#each topics as topic}
+								<div class="form-control flex-row">
+									<!-- svelte-ignore a11y-label-has-associated-control -->
+									<label class="cursor-pointer label space-x-4">
+										<div>
+											<input
+												type="checkbox"
+												name="tag"
+												bind:group={$formValue.tag}
+												value={topic}
+												checked={true}
+												class="radio checkbox checkbox-secondary"
+											/>
+											<span class="checkbox-mark" />
+										</div>
+										<span class="label-text capitalize">{topic}</span>
+									</label>
+								</div>
+							{/each}
+						</div>
+					</div>
 				</div>
 			</div>
 			<hr />
