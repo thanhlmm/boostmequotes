e4d0482f1b143e106fd0d409be85ff187822ad8f
Thanh Le <mihthanh27@gmail.com>
2021-06-15 21:27:12 +0700

    Test

---
+++ src/routes/test.svelte
@@ -0,0 +1,14 @@
+<script>
+	// Logic code here
+	let name = 'thanh';
+
+	const changeName = () => {
+		name = 'Binh';
+	};
+</script>
+
+<div class="p-3 bg-red-400">
+	Hello world {name}
+	<hr />
+	<button on:click={changeName}>change name</button>
+</div>
