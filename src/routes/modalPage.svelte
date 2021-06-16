bd8d03ffc2af477f2c56fd39199ff8de2b007b21
Binh Nguyen <binhq6@gmail.com>
2021-06-16 14:00:33 +0700

    modalPage

---
+++ src/routes/modalPage.svelte
@@ -0,0 +1,21 @@
+<script>
+	let name = 'Christmas';
+</script>
+
+<div class="container flex flex-col h-screen items-center justify-center text-center">
+	<div class="m-0 p-0 text-white">
+		<h1 class="text">Marry {name}</h1>
+		<h1 class="text">and Happy New Year!</h1>
+	</div>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=Charm&display=swap');
+	.container {
+		background: url('https://i.ibb.co/1sQXJ4z/page-bg.png') no-repeat center center/cover;
+	}
+	.text {
+		font-family: 'Charm', cursive;
+		font-size: 6rem;
+	}
+</style>
