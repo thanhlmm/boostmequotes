611cabc06f5ccc2f48ce0a9562fb30c20e106886
Thanh Le <mihthanh27@gmail.com>
2021-07-04 14:02:27 +0700

    Typo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -354,7 +354,7 @@
 			<p class="text-gray-500 max-w-md text-lg">
 				Get your 💩 done by showing inspirational quotes randomly.
 				<br />
-				Gain your engergy back.
+				Gain your energy back.
 			</p>
 		</div>
 		<div class="w-full md:w-2/3 md:pl-4">
